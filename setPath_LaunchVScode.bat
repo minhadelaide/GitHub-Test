d1eed1b851b16bf841d1a207a19478fc19bf94fd
minhadelaide <minhadelaide@gmail.com>
2022-06-13 11:55:53 +0930

    Second commit

---
+++ setPath_LaunchVScode.bat
@@ -0,0 +1,8 @@
+@set GH=\C:\utils\portableGit
+@set PATH=%GH%\bin;%GH%\usr\bin;%GH%\mingw64\bin;%GH%\mingw64\libexec\git-core;%PATH%
+::
+@set PATH=%PATH%;C:\utils\node-v16;C:\utils\vscode1.67.2
+@set SET "JAVA_HOME=C:\utils\portableJDK15\app"
+::
+code .
+::@cmd.exe /K
